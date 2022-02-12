f85b5af6407f5b5748bc2c30420c5517448a6c02
clvsit <879646529@qq.com>
2022-02-12 17:29:42 +0800

    modify FileListComp add search function

--- src/components/container_comp/FileListComp.vue
+++ src/components/container_comp/FileListComp.vue
@@ -2,10 +2,26 @@
     <div class="file-block">
         <h3 class="text-center">文件列表</h3>
         <hr/>
-        <div @click="selectFileItem" class="file-list list-group">
-            <div class="list-group-item file-item" v-for="(item, index) in fileList" :key=index :data-index="index">
+        <div class="func-area">
+            <div class="form form-inline">
+                <div class="form-group">
+                    <label for="fileName">名称：</label>
+                    <input v-model="search.name" type="text" class="form-control file-name-input" id="fileName" placeholder="检索的文件名称">
+                    <i @click="searchFile" class="glyphicon glyphicon-search func-search"></i>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div @click="selectFileItem" id="fileList" class="file-list list-group">
+            <div class="list-group-item file-item" v-for="(item, index) in file.search" :key=index :data-index="index">
                 <div class="file-name" v-text="item.name"></div>
             </div>
+        </div>
+        <div class="file-footer">
+            <b>当前文件数：</b>
+            <span v-text="file.search.length"></span>&nbsp;&nbsp;
+            <b>总文件数：</b>
+            <span v-text="file.list.length"></span>
         </div>
     </div>
 </template>
@@ -14,8 +30,36 @@
 export default {
     name: "FileList",
     emits: ["selectFile"],
-    props: ["fileList"],
+    data() {
+        return {
+            search: {
+                name: ""
+            },
+            file: {
+                list: [],
+                search: []
+            }
+        }
+    },
     methods: {
+        searchFile() {
+            console.log(this.search.name);
+            if (this.search.name == "") {
+                this.file.search = this.file.list;
+            } else {
+                this.file.search = [];
+
+                for (let idx = 0; idx < this.file.list.length; idx++) {
+                    if (this.file.list[idx].name.indexOf(this.search.name) !== -1) {
+                        this.file.search.push(this.file.list[idx]);
+                    }
+                }
+            }
+        },
+        setFileList(fileList) {
+            this.file.list = fileList;
+            this.file.search = fileList;
+        },
         selectFileItem(event) {
             const $fileItem = $(event.target).parents(".file-item"),
                 index = $fileItem.data("index");
@@ -33,21 +77,35 @@ export default {
 
 <style lang="scss" scoped>
 .file-block {
+    position: relative;
     width: 300px;
-    height: 100%;
+    min-height: 550px;
     padding: 5px 10px;
     border-radius: 5px;
     box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.5);
     background-color: #fff;
 
+    .func-area {
+        width: 100%;
+        height: 40px;
+
+        .file-name-input {
+            width: 200px;
+        }
+
+        .func-search {
+            margin-left: 10px;
+            cursor: pointer;
+        }
+    }
+
     .file-list {
-        height: 790px;
+        height: 300px;
         overflow: auto;
 
         .file-item {
             position: relative;
-            width: 100%;
-            min-width: 280px;
+            width: 276px;
             height: 40px;
             cursor: pointer;
             transition: all 0.5s ease-in-out;
@@ -86,6 +144,12 @@ export default {
         box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
         border-radius: 0;
         background: rgba(0,0,0,0.1);
+    }
+
+    .file-footer {
+        height: 50px;
+        line-height: 50px;
+        border-top: 1px solid rgba(0, 0, 0, 0.1);
     }
 
 }
